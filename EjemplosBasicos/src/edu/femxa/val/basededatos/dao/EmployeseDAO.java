137b4d1515331ce98768fe136cb2eb01506b372c
valexx55 <valexx55@gmail.com>
2017-01-26 18:24:13 +0100

    Ejemplos DAO

---
+++ EjemplosBasicos/src/edu/femxa/val/basededatos/dao/EmployeseDAO.java
@@ -0,0 +1,29 @@
+package edu.femxa.val.basededatos.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeseDAO {
+	
+	public List<EmployeesDTO> consultaAllEmpleados ()
+	
+	{
+		List<EmployeesDTO> lista_e = null;
+		
+			lista_e = new ArrayList<EmployeesDTO>();
+		
+			//lista_e.add(new EmployeesDTO(idEmpleado, firstName, salary, dpto))
+			//TODO 
+		/**
+		 * Conectarme a la base de datos
+		 * ejecutar la consulta
+		 * para cada registro, 
+		 * 	crear un objeto empleadosDTO
+		 * 	add ese DTO a la lista
+		 * librerar Recursos
+		 */
+		
+		return lista_e;
+	}
+
+}
