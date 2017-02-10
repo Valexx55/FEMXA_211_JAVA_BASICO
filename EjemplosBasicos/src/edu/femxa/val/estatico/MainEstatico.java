775ca7d0ccdc9eed1247da238b16c14b57f6ec1a
valexx55 <valexx55@gmail.com>
2017-02-10 20:30:35 +0100

    Sección Static

---
+++ EjemplosBasicos/src/edu/femxa/val/estatico/MainEstatico.java
@@ -0,0 +1,11 @@
+package edu.femxa.val.estatico;
+
+public class MainEstatico {
+	
+	public static void main(String[] args) {
+		
+		EjemploEstatico e1 = new EjemploEstatico();
+		EjemploEstatico e2 = new EjemploEstatico();
+	}
+
+}
